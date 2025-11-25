df8d1ffcebb6d88b69b3160858d862b948f613ec
Matias Scalella <matias.scalella@gmail.com>
2025-11-25 20:04:09 -0300

    Fix BookDAO SQL errors: add missing commas, include br_book_id in SELECTs, and correct SELECT_BY_ID/SELECT_ALL syntax.

--- src/dao/BookDAO.java
+++ src/dao/BookDAO.java
@@ -16,8 +16,9 @@ import java.util.ArrayList;
 public class BookDAO implements GenericDAO<Book> {
     
 // ----------- SQL statements
-    private static final String INSERT_SQL = "INSERT INTO books (deleted, title, author, publisher, publication_year" +
-            "VALUES (?, ?, ?, ?, ?, ?)";
+    private static final String INSERT_SQL =
+        "INSERT INTO books (deleted, title, author, publisher, publication_year) " +
+        "VALUES (?, ?, ?, ?, ?)";
     
     private static final String SELECT_BY_ID_SQL =
         "SELECT " +
@@ -32,7 +33,8 @@ public class BookDAO implements GenericDAO<Book> {
         "  br.isbn AS br_isbn, " +
         "  br.dewey_class AS br_dewey_class, " +
         "  br.shelf_location AS br_shelf_location, " +
-        "  br.language AS br_language " +
+        "  br.language AS br_language, " +
+        "  br.book_id AS br_book_id " +
         "FROM books b " +
         "LEFT JOIN bibliographic_records br " +
         "       ON br.book_id = b.id " +
@@ -41,18 +43,19 @@ public class BookDAO implements GenericDAO<Book> {
     
     private static final String SELECT_ALL_SQL =
         "SELECT " +
-        "  b.id              AS book_id, " +
-        "  b.deleted         AS book_deleted, " +
-        "  b.title           AS book_title, " +
-        "  b.author          AS book_author, " +
-        "  b.publisher       AS book_publisher, " +
+        "  b.id AS book_id, " +
+        "  b.deleted AS book_deleted, " +
+        "  b.title AS book_title, " +
+        "  b.author AS book_author, " +
+        "  b.publisher AS book_publisher, " +
         "  b.publication_year AS book_publication_year, " +
-        "  br.id             AS br_id, " +
-        "  br.deleted        AS br_deleted, " +
-        "  br.isbn           AS br_isbn, " +
-        "  br.dewey_class    AS br_dewey_class, " +
+        "  br.id AS br_id, " +
+        "  br.deleted AS br_deleted, " +
+        "  br.isbn AS br_isbn, " +
+        "  br.dewey_class AS br_dewey_class, " +
         "  br.shelf_location AS br_shelf_location, " +
-        "  br.language       AS br_language " +
+        "  br.language AS br_language, " +
+        "  br.book_id AS br_book_id " +
         "FROM books b " +
         "LEFT JOIN bibliographic_records br " +
         "       ON br.book_id = b.id " +
@@ -76,7 +79,7 @@ public class BookDAO implements GenericDAO<Book> {
     @Override
     public Book findById(Long id) throws SQLException {
         if (id == null) {
-            throw new IllegalArgumentException("ID cannot be null");
+            throw new IllegalArgumentException("Book ID cannot be null");
         }
         try (Connection conn = DatabaseConnection.getConnection()) {
             return findById(id, conn);
@@ -141,6 +144,9 @@ public class BookDAO implements GenericDAO<Book> {
      * Finds a Book by ID using an existing Connection.
      */
     public Book findById(Long id, Connection conn) throws SQLException {
+        if (id == null) {
+            throw new IllegalArgumentException("ID cannot be null");
+        }
         try (PreparedStatement ps = conn.prepareStatement(SELECT_BY_ID_SQL)) {
             ps.setLong(1, id);
             try (ResultSet rs = ps.executeQuery()) {
@@ -207,41 +213,47 @@ public class BookDAO implements GenericDAO<Book> {
     * Builds a Book (and its optional BibliographicRecord)
     * from the current row of the ResultSet.
     */
-   private Book buildBookFromResultSet(ResultSet rs) throws SQLException {
-       Book book = new Book();
+    private Book buildBookFromResultSet(ResultSet rs) throws SQLException {
+        Book book = new Book();
 
-       // ------ Book fields ------
-       book.setId(rs.getLong("book_id"));
-       book.setDeleted(rs.getBoolean("book_deleted"));
-       book.setTitle(rs.getString("book_title"));
-       book.setAuthor(rs.getString("book_author"));
-       book.setPublisher(rs.getString("book_publisher"));
+        // ------ Book fields ------
+        book.setId(rs.getLong("book_id"));
+        book.setDeleted(rs.getBoolean("book_deleted"));
+        book.setTitle(rs.getString("book_title"));
+        book.setAuthor(rs.getString("book_author"));
+        book.setPublisher(rs.getString("book_publisher"));
 
-       int year = rs.getInt("book_publication_year");
-       if (rs.wasNull()) {
-           book.setPublicationYear(null);
-       } else {
-           book.setPublicationYear(year);
-       }
+        int year = rs.getInt("book_publication_year");
+        if (rs.wasNull()) {
+            book.setPublicationYear(null);
+        } else {
+            book.setPublicationYear(year);
+        }
 
-       // ------ BibliographicRecord (optional) ------
-       Long brId = rs.getLong("br_id");
+        // ------ BibliographicRecord (optional) ------
+        Long brId = rs.getLong("br_id");
 
-       if (!rs.wasNull()) {
-           BibliographicRecord br = new BibliographicRecord();
-           br.setId(brId);
-           br.setDeleted(rs.getBoolean("br_deleted"));
-           br.setIsbn(rs.getString("br_isbn"));
-           br.setDeweyClass(rs.getString("br_dewey_class"));
-           br.setShelfLocation(rs.getString("br_shelf_location"));
-           br.setLanguage(rs.getString("br_language"));
+        if (!rs.wasNull()) {
+            BibliographicRecord br = new BibliographicRecord();
+            br.setId(brId);
+            br.setDeleted(rs.getBoolean("br_deleted"));
+            br.setIsbn(rs.getString("br_isbn"));
+            br.setDeweyClass(rs.getString("br_dewey_class"));
+            br.setShelfLocation(rs.getString("br_shelf_location"));
+            br.setLanguage(rs.getString("br_language"));
+            long fk = rs.getLong("br_book_id");
+            if (rs.wasNull()) {
+                br.setBookId(null);
+            } else {
+                br.setBookId(fk);
+            }
 
-           book.setBibliographicRecord(br);
-       } else {
-           book.setBibliographicRecord(null);
-       }
+            book.setBibliographicRecord(br);
+        } else {
+            book.setBibliographicRecord(null);
+        }
 
-       return book;
-   }
+        return book;
+    }
     
 }
